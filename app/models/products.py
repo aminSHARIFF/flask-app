9906a099beb4c803565e92ddce63da3a9c75ad5d
bamser3 <wunpun3@gmail.com>
2026-04-20 19:15:13 +0300

    clean repo (remove temp + ignored files)

---
+++ app/models/products.py
@@ -0,0 +1,14 @@
+from app import db
+from app.models.base import BaseModel
+
+class Product(BaseModel):
+    __tablename__ = "products"
+    
+    name = db.Comlumn(db.String, nullable = False)
+    price = db.Column(db.Float, nullable = False)
+    stock = db.Column(db.Integer, nullable = False)
+    
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
+    supplier_id = db.Column(db.Integer, db.ForeignKey("suppliers.id"))
+    
+    transactions = db.relationship("InventoryTransaction", back_populates="product")
